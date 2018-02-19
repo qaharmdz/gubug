11c42e3e085b2ede5989d48c34b4069532e9309f
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2018-02-19 23:01:58 +0700

    remove hhvm from travis and add tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 
 $loader = require realpath(__DIR__ . '/../vendor/') . DIRECTORY_SEPARATOR . 'autoload.php';
-$loader->addPsr4('Contoh\\', realpath(__DIR__ . '/Demo/'));
+$loader->addPsr4('Gubug\Test\\', realpath(__DIR__ . '/src/'));
---
+++ tests/src/Library/ConfigTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace Gubug\Test\Library;
+
+class ConfigTest extends \PHPUnit\Framework\TestCase
+{
+    protected $config;
+
+    protected function setUp()
+    {
+        $this->config = new \Gubug\Library\Config();
+    }
+
+    protected function tearDown()
+    {
+        $this->config = null;
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Gubug\Library\Config', $this->config);
+    }
+
+    public function testSetGet()
+    {
+        $this->config->set('foo', 'bar');
+        $this->config->set('baz', ['foo' => 'bar', 'world' => 'cool']);
+
+        $this->assertSame('bar', $this->config->get('foo'));
+        $this->assertSame('bar', $this->config->get('baz.foo')); // dot-notation
+    }
+
+    public function testGetDefault()
+    {
+        $this->assertSame('world', $this->config->get('foo', 'world'));
+        $this->assertSame('cool', $this->config->get('baz.foo', 'cool'));
+    }
+
+    public function testGetArray()
+    {
+        $this->config->set('foo', 'bar');
+
+        $this->assertEquals(['bar'], $this->config->getArray('foo'));
+    }
+
+    public function testRemove()
+    {
+        $this->config->set('foo', 'bar');
+        $this->config->set('baz', ['foo' => 'bar', 'world' => 'cool']);
+
+
+        $this->config->remove('foo');
+        $this->config->remove('baz.world');
+        $this->config->remove('baz.foo.cool'); // do nothing
+
+        $this->assertSame('world', $this->config->get('foo', 'world'));
+        $this->assertSame('fallback', $this->config->get('baz.world', 'fallback'));
+
+        $this->assertSame('bar', $this->config->get('baz.foo', 'not-removed-from-baz'));
+    }
+}
